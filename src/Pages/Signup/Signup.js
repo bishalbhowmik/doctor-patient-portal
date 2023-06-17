373931d83014d8b3f661902ec4a60b5913707ef0
bishalbhowmik <77568117+bishalbhowmik@users.noreply.github.com>
2023-06-17 19:00:37 +0600

    Work on signup page

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { useForm } from "react-hook-form"
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
+const Signup = () => {
+
+    const {register,handleSubmit, formState:{errors}} = useForm();
+
+    const handleSignup = data =>console.log(data);
 
 
-const Login = () => {
-
-    const { register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
-
-    const onSubmit = (data) => console.log(data);
     return (
 
-
         <div className='flex flex-col items-center justify-center h-[100vh]'>
-            <form className='shadow-lg p-10 rounded w-[500px]' onSubmit={handleSubmit(onSubmit)}>
-                <h2 className='text-black text-center text-3xl'>Login</h2>
+            <form className='shadow-lg p-10 rounded w-[500px]' onSubmit={handleSubmit(handleSignup)}>
+                <h2 className='text-black text-center text-3xl'>Sign Up</h2>
 
+
+                <div>
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" {...register("name", {
+                        required: "This field is required"
+                    })} name="name" className="input input-bordered w-full" />
+                    {errors.name && <p className='text-gray-500'>{errors.name?.message}</p>}
+                </div>
 
                 <div>
                     <label className="label">
@@ -27,7 +33,7 @@ const Login = () => {
                     <input type="email" {...register("email", {
                         required: "This field is required"
                     })} name="email" className="input input-bordered w-full" />
-                    {errors.email && <p>{errors.email?.message}</p>}
+                    {errors.email && <p className='text-gray-500'>{errors.email?.message}</p>}
                 </div>
 
 
@@ -40,13 +46,13 @@ const Login = () => {
                         required: "This field is required",
                         minLength: { value: 6, message: 'Password must be 6 character long' }
                     })} name="password" className="input input-bordered w-full" />
-                    {errors.password && <p>{errors.password?.message}</p>}
+                    {errors.password && <p className='text-gray-500'>{errors.password?.message}</p>}
 
                 </div>
 
-                <button className='btn btn-accent w-full'>Login</button>
+                <button className='btn btn-accent w-full'>SignUp</button>
 
-                <h3 className='text-xs mt-4 w-[90%] text-center'>New to Doctors Portal? <Link to='/signup' className='text-secondary'>Create New Account</Link></h3>
+                <h3 className='text-xs mt-4 w-[90%] text-center'>Already have an account? <Link to='/login' className='text-secondary'>Please Log In</Link></h3>
 
                 <div className="flex flex-col w-full border-opacity-50">
 
@@ -63,4 +69,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,6 +3,7 @@ import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import AppointmentPage from "../../Pages/AppointmentPage/AppointmentPage/AppointmentPage";
+import Signup from "../../Pages/Signup/Signup";
 
  const router = createBrowserRouter([
     {
@@ -18,9 +19,14 @@ import AppointmentPage from "../../Pages/AppointmentPage/AppointmentPage/Appoint
                 element:<Login></Login>
             },
             {
+                path:'/signup',
+                element:<Signup></Signup>
+            },
+            {
                 path:'/appointment',
                 element:<AppointmentPage></AppointmentPage>
             }
+
         ]
     }
 ])
